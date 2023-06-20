b9a3f0076577fffe50473a94995d40a1f6b96951
lucas <lucaslion7@gmail.com>
2023-06-20 10:07:33 +0200

    m1-ex04 unfinished

--- m1/ex04/main.cpp
+++ m1/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: llion <llion@student.42mulhouse.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 15:59:49 by llion             #+#    #+#             */
-/*   Updated: 2023/06/19 17:03:36 by llion            ###   ########.fr       */
+/*   Updated: 2023/06/19 18:18:09 by llion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,17 @@ int main(int argc, char **argv)
 {
 	if (argc == 4) {
 		std::ifstream ollivander(argv[1]);
+		std::ofstream newFile(std::string(argv[1]) + ".replace");
 		std::string word;
+		std::cout << ollivander.rdbuf() << std::endl;
 		if (ollivander) {
 			while (ollivander >> word)
 				if (word == argv[2]) {
 					word = argv[3];
 				}
-		std::cout << ollivander.gcount() << std::endl;
-			ollivander.seekg(0, ollivander.beg);
-			std::cout << ollivander.rdbuf() << std::endl;
-			ollivander.close();
+				newFile << word << " ";
+				ollivander.close();
+				newFile.close();
 		}
 		else
 			std::cout << "Error: Fatal" << std::endl;
