2cdaaa4778fcbda8a1ee112305c9dd4a9721a202
lucas <lucaslion7@gmail.com>
2023-07-11 13:26:54 +0200

    m2-ex01 ok

--- m2/ex01/Fixed.cpp
+++ m2/ex01/Fixed.cpp
@@ -6,11 +6,12 @@
 /*   By: llion <llion@student.42mulhouse.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 23:28:48 by llion             #+#    #+#             */
-/*   Updated: 2023/07/07 16:08:31 by llion            ###   ########.fr       */
+/*   Updated: 2023/07/11 13:10:09 by llion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+#include "math.h"
 
 Fixed::Fixed() {
 	this->_fixNumber = 0;
@@ -32,14 +33,12 @@ Fixed::Fixed( const Fixed& source ) {
 
 Fixed::Fixed( int const num ) {
 	std::cout << "Int constructor called" << std::endl;
-	//this->_fixNumber = num << Fixed::_fract; 
-	this->_fixNumber = num;
+	this->_fixNumber = num << Fixed::_fract; 
 }
 
 Fixed::Fixed( float const num ) {
 	std::cout << "Float constructor called" << std::endl;
-	//this->_fixNumber = roundf(num * (1 << Fixed::_fract));
-	this->_fixNumber = num;
+	this->_fixNumber = roundf(num * (1 << Fixed::_fract));
 }
 
 Fixed::~Fixed() {
@@ -58,9 +57,9 @@ std::ostream&		operator<<( std::ostream& COUT, const Fixed& f ) {
 void	Fixed::setRawBits( int const raw ) { this->_fixNumber = raw; }
 
 float	Fixed::toFloat( void ) const { 
-	//return ((float)this->_fixNumber / (float)(1 << Fixed::_fract));
+	return ((float)this->_fixNumber / (float)(1 << Fixed::_fract));
 	return (static_cast<float>(this->getRawBits()));
 }
 
-int		Fixed::toInt( void ) const { return (static_cast<int>(this->_fixNumber)); }
+int		Fixed::toInt( void ) const { return (this->_fixNumber >> Fixed::_fract); }
 
