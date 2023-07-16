6d250d2becf65bf889c31662ec025e1d6c20e7e4
lucas <lucaslion7@gmail.com>
2023-07-16 13:17:41 +0200

    m3-ex00 ok

--- m3/ex00/ClapTrap.cpp
+++ m3/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: llion@student.42mulhouse.fr <marvin@42.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 15:01:33 by llion@student     #+#    #+#             */
-/*   Updated: 2023/07/14 17:42:57 by llion@student    ###   ########.fr       */
+/*   Updated: 2023/07/16 13:11:37 by llion@student    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,31 @@
 #include <algorithm>
 
 ClapTrap::ClapTrap( ) {
-	std::cout << "Default constructor called" << std::endl;
+	std::cout << "\033[32mDefault constructor called\033[0m" << std::endl;
 }
 
 ClapTrap::ClapTrap( const std::string name ) : _name(name), _health(10), _energy(10), _damage(0){
-	std::cout << "Constructor called" << std::endl;
+	std::cout << "\033[32mConstructor called\033[0m" << std::endl;
 }
 
 ClapTrap::ClapTrap( const ClapTrap& source ) {
-	(void)source;
-	std::cout << "Copy constructor called" << std::endl;
+	std::cout << "\033[32mCopy constructor called\033[0m" << std::endl;
+	*this = source;
 }
 
 ClapTrap&	ClapTrap::operator=( const ClapTrap& source ) {
-	(void)source;
+	if (this != &source) {
+		std::cout << "\033[32mAssignation operator called\033[0m" << std::endl;
+		this->setName(source.getName());
+		this->setHealth(source.getHealth());
+		this->setEnergy(source.getEnergy());
+		this->setDamage(source.getDamage());
+	}
 	return (*this);
 }
 
 ClapTrap::~ClapTrap( ) {
-	std::cout << "Destructor called" << std::endl;
+	std::cout << "\033[31mDestructor called\033[0m" << std::endl;
 }
 
 std::string	ClapTrap::getName( void ) const { return this->_name;} 
@@ -52,14 +58,30 @@ void	ClapTrap::setEnergy( int energy ) { this->_energy = energy; };
 void	ClapTrap::setDamage( int damage ) { this->_damage = damage; };
  
 void	ClapTrap::attack( const std::string& target ) {
-	std::cout << this->getName() << " attacks " << target << ", causing " << this->getDamage() << " points of damage!" << std::endl;
+	std::cout << this->getName() << " attacks " << target << ", causing \033[31m" << this->getDamage() << "\033[0m points of damage!" << std::endl;
 }
 
 void	ClapTrap::takeDamage( unsigned int amount ) {
-	std::cout << this->getName() << " takes " << amount << " damages!" << std::endl;
+	if (this->getEnergy() > 0 && this->getHealth() > 0) {
+		std::cout << this->getName() << " takes \033[31m" << amount << "\033[0m damages!" << std::endl;
+		this->setHealth(std::max(this->getHealth() - (int)amount, 0));
+		this->setEnergy(std::max(this->getEnergy() - 1, 0));
+	}
+	else if (this->getHealth() <= 0)
+		std::cout << this->getName() << " tries to repair but " << this->getName() << " is \033[31m dead!\033[0m" << std::endl;
+	else
+		std::cout << this->getName() << " tries to repair but "  << " is out of energy!" << std::endl;
 }
 
 void	ClapTrap::beRepaired( unsigned int amount ) {
-	std::cout << this->getName() << " gets " << amount << " health " << std::endl;
+	if (this->getEnergy() > 0 && this->getHealth() > 0) {
+		std::cout << this->getName() << " gets \033[32m" << amount << "\033[0m health back!" << std::endl;
+		this->setHealth(std::min(this->getHealth() + (int)amount, 100));
+		this->setEnergy(std::max(this->getEnergy() - 1, 0));
+	}
+	else if (this->getHealth() <= 0)
+		std::cout << this->getName() << " tries to repair but " << this->getName() << " is\033[31m dead\033[0m!" << std::endl;
+	else 
+		std::cout << this->getName() << " tries to repair but "  << " is \033[31mout of energy\033[0m!" << std::endl;
 }
 
--- m3/ex00/main.cpp
+++ m3/ex00/main.cpp
@@ -6,17 +6,55 @@
 /*   By: llion@student.42mulhouse.fr </var/spool/m  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 16:34:24 by llion@student     #+#    #+#             */
-/*   Updated: 2023/07/14 17:35:06 by llion@student    ###   ########.fr       */
+/*   Updated: 2023/07/16 13:16:14 by llion@student    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
+#define RE "\033[0m"
+#define R "\033[31m"
+#define G "\033[32m"
+#define Y "\033[33m"
+#define B "\033[34m"
+
 int main()
 {
 	ClapTrap clap("ted");
-	clap.attack("target");
-	clap.takeDamage(10);
+	ClapTrap clap_copy;
+	clap_copy = clap;
+	ClapTrap clap_copy2(clap);
+
+	std::cout << std::endl;
+	std::cout << "clap name: " << B << clap.getName() << RE << std::endl;
+	std::cout << "clap health: " << B << clap.getHealth() << RE << std::endl;
+	std::cout << "clap energy: " << B << clap.getEnergy() << RE << std::endl;
+	std::cout << "clap damages: " << B << clap.getDamage() << RE << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "clap_copy name: " << B << clap_copy.getName() << RE << std::endl;
+	std::cout << "clap_copy health: " << B << clap_copy.getHealth() << RE << std::endl;
+	std::cout << "clap_copy energy: " << B << clap_copy.getEnergy() << RE << std::endl;
+	std::cout << "clap_copy damages: " << B << clap_copy.getDamage() << RE << std::endl;
+
+	std::cout << std::endl;
+	clap_copy.takeDamage(1);
+	std::cout << "clap_copy health: " << clap_copy.getHealth() << std::endl;
+	std::cout << "clap health: " << clap.getHealth() << std::endl;
+	clap_copy.beRepaired(1);
+	std::cout << "clap_copy health: " << clap_copy.getHealth() << std::endl;
+	std::cout << "clap health: " << clap.getHealth() << std::endl;
+	clap_copy.takeDamage(10);
+	std::cout << "clap_copy health: " << clap_copy.getHealth() << std::endl;
+	std::cout << "clap health: " << clap.getHealth() << std::endl;
+	clap_copy.beRepaired(10);
 	clap.beRepaired(10);
+	clap.attack("Mike");
+
+	clap_copy2.attack("Sebastian");
+	clap_copy2.takeDamage(9);
+	clap_copy2.takeDamage(9);
+	clap_copy2.beRepaired(100);
+	std::cout << std::endl;
 	return (0);
 }
